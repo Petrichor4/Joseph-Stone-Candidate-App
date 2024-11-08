e05115830c50e26ab478dff83df65f018730466f
Kierstyn Hart <stonepreston05@gmail.com>
2024-11-08 12:07:35 -0700

    made the function to fetch user

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,12 +1,48 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+  const [cand, setcand] = useState<Candidate>({
+    img: '',
+    login: '',
+    location: '',
+    company: '',
+    bio: '',
+  })
+  const getCand = async () => {
+    const data: Candidate = await searchGithub();
+    console.log(data);
+    setcand({
+      img: data.img,
+      login: data.login,
+      location: data.location,
+      company: data.company,
+      bio: data.bio,
+    });
+  }
+  const saveCand = async () => {
+    const data: Candidate = await searchGithubUser(cand.login);
+    const savedCand = JSON.parse(localStorage.getItem('savedCand') || '[]');
+    savedCand.push(data);
+    localStorage.setItem('savedCand', JSON.stringify(savedCand));
+  }
   // Create component to handle the search for a list of employees that is randomly generated then selecting just one of the employees from that list=> 
   // function to handle the mouse clicks =>
   // Create component to display the information in a card on the screen =>
   return (
-  <h1>CandidateSearch</h1>
+  <div id='cand-display'>
+    <h1>CandidateSearch</h1>
+    <section id='cand'>
+      <img src={`${cand.img}`} alt={cand.login} />
+      <h2>{cand.login}</h2>
+      <h3>{cand.location}</h3>
+      <h4>{cand.company}</h4>
+      <p>{cand.bio}</p>
+      <button onClick={getCand}>Get Candidate</button>
+      <button onClick={saveCand}>Save Candidate</button>
+    </section>
+  </div>
 );
 };
 
